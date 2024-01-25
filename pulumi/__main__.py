d7e7e39ad0eebd958bf8e7c213e6a5e565861577
Martin <mvalencia1350@arizona.edu>
2024-01-25 15:34:26 -0700

    Add west-2 aws region to main.py

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -1,10 +1,14 @@
 """An AWS Python Pulumi program"""
 
 import pulumi
-from pulumi_aws import s3
+#from pulumi_aws import s3
+import pulumi_aws as aws
 
 # Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+bucket = aws.s3.Bucket('my-bucket')
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
+
+# Override the AWS region for the deployment.
+aws.config.region = 'us-west-2'
